39b48c8e90d88369d7210b587b8d9d4317774ee9
Ale <alejdelcomp@gmail.com>
2019-09-23 18:05:25 -0600

    Finished Portal
    Also marked things as done in README

--- soup.py
+++ soup.py
@@ -21,37 +21,73 @@ def separator_items():
     
 def separator_parts():
     print ("=============================================================================================================================")
-
-def out_txt():
-    print ("Output exceeds 30 lines, sending output to: <logfile>")
-    return "Hello World"
-
-def easyprint (element,name):
-  for each_div in soup.findAll(element,{'class':name}):
-      print (each_div)
-          
+     
 def portal():
+    print("Alejandra Lemus")
     separator_parts()
+    ##TITLE
     print ("GET the title and print it: "+soup.title.string)
-    separator_items
+    separator_items()
+    
+    ##ADDRESS
     address=soup.find("a", {"href": "#myModal"})   
     print("GET the Complete Address of UFM: "+address.text)
-    separator_items
+    separator_items()
+    
+    ##EMAIL AND PHONE NUMBER
     phone=soup.find("a", {"href": "tel:+50223387700"})
     email=soup.find("a", {"href": "mailto:inf@ufm.edu"})   
-    print("GET the phone number and info email:"+phone+email)
-    separator_items
+    print(f"GET the phone number: <{phone.text}> and info email: "+email.text)
+    separator_items()
+         
+    ##UPPER NAV MENU   
     print("GET all item that are part of the upper nav menu (id: menu-table):")
-    separator_items
-    print("find all properties that have href (link to somewhere):")
-    separator_items
+    navmenu=soup.find_all("table", {"id": f"menu-table"})
+    for items in navmenu:
+        for navmenuitems in items.find_all("div"):
+            menuitems=navmenuitems.string
+            if menuitems is not None:
+                    menuitems = str(menuitems).strip()
+                    print(f"- {menuitems}")
+       
+    ##HREF             
+    separator_items()
+    print("find all properties that have href (link to somewhere):") 
+    file = open("portal_find_all_properties_that_have_href.txt","w")
+    print ("Output exceeds 30 lines, sending output to: <portal_find_all_properties_that_have_href.txt>")
+    hrefs = []
+    for refs in soup.find_all('a', href=True):
+        hrefs.append(refs['href'])
+        
+    for item in hrefs:
+        file.write("%s\n" % item)
+    file.close()  
+    
+    ##UFMAIL
+    separator_items()
     print("GET href of 'UFMail' button:")
-    separator_items
+    mailButton=soup.find(id="ufmail_")['href']
+    print(f"{mailButton}")
+    
+    ##MIU
+    separator_items()
     print("GET href 'MiU' button:")
-    separator_items
-    print("get hrefs of all <img>")
-    separator_items
-    print("count all <a>:")
+    miuButton=soup.find(id="ufmail_")['href']
+    print(f"{miuButton}")
+    
+    ##<IMG>
+    separator_items()
+    print("get hrefs of all <img>:")
+    imgs=soup.find_all('img')
+    for img in imgs:
+        print (img['src'])
+
+    ##<A>
+    separator_items()
+    count=0
+    for a in soup.find_all("a"):
+        count=count+1
+    print("count all <a>: "+str(count))
  
 def estudios():
     separator_parts()
@@ -68,9 +104,4 @@ def estudios():
 def cs():
     separator_parts()
     
-output = out_txt()
-file = open("sample.txt","w")
-file.write(output)
-file.close()
-
 portal()
