4e2e8fb538fb947552954e221999cf56cd9a4a64
Ale <alejdelcomp@gmail.com>
2019-09-23 23:36:14 -0600

    2/3 finished

--- directorio.py
+++ directorio.py
@@ -22,8 +22,29 @@ def separator_items():
     
 def separator_parts():
     print ("=============================================================================================================================")
-     
+
 def directorio():
     separator_parts()
     
+    ##EMAILS
+    print("Sort all emails alphabetically (href='mailto:arquitectura@ufm.edu') in a list, dump it to logs/4directorio_emails.txt")
+    file = open("logs/4directorio_emails.txt","w")
+    mails=[]
+    for datamails in soup.find_all(attrs={"href":True}):
+            if str(datamails['href']).__contains__("mailto"):
+                mails.append(str(datamails['href'].replace("mailto:","")))
+    
+    mails.sort()
+    mails=list(dict.fromkeys(mails))
+    for item in mails:
+        file.write("%s\n" % item)
+    file.close()  
+    
+    ##ALPHABET
+    words = mails
+    words_starting_with_vowel = [word for word in words if word[0] in 'aeiou']
+    count=0
+    for a in words_starting_with_vowel:
+        count=count+1
+    print("Count all emails that start with a vowel: "+str(count))
 directorio()
